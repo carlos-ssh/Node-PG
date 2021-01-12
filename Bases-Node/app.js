d68c06ffc2a0b218bed419b68a102c5268fbc47e
Carlos-SSH <carlos.anezeh@me.com>
2021-01-12 13:48:00 -0600

    create a Readme file.

--- Bases-Node/app.js
+++ Bases-Node/app.js
@@ -1,4 +1,6 @@
 const argv = require('./config/yargs').argv;
+const colors = require('colors');
+
 const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
 
@@ -11,13 +13,13 @@ switch (comando) {
 
     case 'crear':
         crearArchivo(argv.base, argv.limite)
-            .then(archivo => console.log(`Archivo Creado: ${ archivo }`))
+            .then(archivo => console.log(`Archivo Creado: ${ archivo.white }`.inverse.green))
             .catch(e => console.log(e));
         console.log('Crear');
         break;
 
     default:
-        console.log(' Comando no reconocido :( ');
+        console.log(' Comando no reconocido :( '.inverse.red);
 }
 
 console.log(argv.base);
