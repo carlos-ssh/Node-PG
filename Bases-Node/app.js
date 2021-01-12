3581891af64f77b15628c4537c4ca92311d68e05
Carlos-SSH <carlos.anezeh@me.com>
2021-01-12 12:51:04 -0600

    Create a new path for optimize yargs.

--- Bases-Node/app.js
+++ Bases-Node/app.js
@@ -1,28 +1,6 @@
-const argv = require('yargs')
-    .command('listar', 'Imprime en consola la table de multiplicar', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10,
-        },
-    })
-    .command('crear', 'Genera un Archivo con la tabla de multiplicar', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10,
-        },
-    })
-    .help()
-    .argv;
-
+const argv = require('./config/yargs').argv;
 const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
 
 let comando = argv._[0];
 
