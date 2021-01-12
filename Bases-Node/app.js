e06591ac9efe21b625ce398b4c7537b0c3135feb
Carlos-SSH <carlos.anezeh@me.com>
2021-01-12 11:47:15 -0600

    Add yargs package to this project and created limits and documentation for this application.

--- Bases-Node/app.js
+++ Bases-Node/app.js
@@ -1,9 +1,27 @@
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la table de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10,
+        }
+    })
+    .help()
+    .argv;
+
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1];
+let argv2 = process.argv;
 
-crearArchivo(base)
-    .then(archivo => console.log(`Archivo Creado: ${ archivo }`))
-    .catch(e => console.log(e));
+console.log('Limite', argv.base);
+
+
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
+
+// crearArchivo(base)
+//     .then(archivo => console.log(`Archivo Creado: ${ archivo }`))
+//     .catch(e => console.log(e));
