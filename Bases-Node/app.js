7497cd3e6e5be104055e0d8f381aa458360d3d0d
Carlos-SSH <carlos.anezeh@me.com>
2021-01-12 12:44:23 -0600

    Crete a limit for tables and create a view in console if you want to watch charts.

--- Bases-Node/app.js
+++ Bases-Node/app.js
@@ -7,21 +7,43 @@ const argv = require('yargs')
         limite: {
             alias: 'l',
             default: 10,
-        }
+        },
+    })
+    .command('crear', 'Genera un Archivo con la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10,
+        },
     })
     .help()
     .argv;
 
-const { crearArchivo } = require('./multiplicar/multiplicar');
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
-let argv2 = process.argv;
+let comando = argv._[0];
 
-console.log('Limite', argv.base);
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo Creado: ${ archivo }`))
+            .catch(e => console.log(e));
+        console.log('Crear');
+        break;
+
+    default:
+        console.log(' Comando no reconocido :( ');
+}
+
+console.log(argv.base);
 
 
 // let parametro = argv[2];
 // let base = parametro.split('=')[1];
-
-// crearArchivo(base)
-//     .then(archivo => console.log(`Archivo Creado: ${ archivo }`))
-//     .catch(e => console.log(e));
--- Bases-Node/multiplicar/multiplicar.js
+++ Bases-Node/multiplicar/multiplicar.js
@@ -1,27 +1,40 @@
 // REQUIREDS
 const fs = require('fs');
 
+let listarTabla = (base, limite = 10) => {
 
-let crearArchivo = (base) => {
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${ base } * ${ i } = ${ base * i }`);
+    }
+
+}
+
+let crearArchivo = (base, limite = 10) => {
     return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido ${ base } no es un numero.`)
+            return;
+        }
 
         let data = '';
 
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 1; i <= limite; i++) {
             data += `${ base } * ${ i } = ${ base * i }\n`;
         }
 
-        fs.writeFile(`./tablas/tabla-${ base }.txt `, data, (err) => {
+        fs.writeFile(`./tablas/tabla-${ base }, al ${ limite }.txt `, data, (err) => {
 
             if (err)
                 reject(err)
             else
-                resolve(`Tabla-${ base } `);
+                resolve(`Tabla-${ base }, al ${ limite }.txt `);
         });
 
     });
 }
 
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listarTabla
 }
